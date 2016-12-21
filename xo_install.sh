a9a83809d4e68bafc0779293d12f87973f619137
Jarli01 <dustinbeuerlein1@gmail.com>
2016-12-21 15:38:04 -0500

    Updated a sudo issue
    minor permission change.

--- xo_install.sh
+++ xo_install.sh
@@ -3,7 +3,7 @@ sudo apt-get install --yes nfs-common
 cd /opt
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install --yes nodejs
-curl -o /usr/local/bin/n https://raw.githubusercontent.com/visionmedia/n/master/bin/n
+sudo curl -o /usr/local/bin/n https://raw.githubusercontent.com/visionmedia/n/master/bin/n
 sudo chmod +x /usr/local/bin/n
 sudo n stable
 sudo apt-get install --yes build-essential redis-server libpng12-dev git python-minimal
