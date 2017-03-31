79e8057e869bce6463da7a16092060c32b7ac38f
Stephanos <smal78@yahoo.com>
2017-03-31 12:14:03 +0300

    Update xo_install.sh
    installing and using  yarn package manager

--- xo_install.sh
+++ xo_install.sh
@@ -7,32 +7,20 @@ sudo curl -o /usr/local/bin/n https://raw.githubusercontent.com/visionmedia/n/ma
 sudo chmod +x /usr/local/bin/n
 sudo n stable
 sudo apt-get install --yes build-essential redis-server libpng-dev git python-minimal
-git clone -b stable https://github.com/vatesfr/xo-server
-git clone -b stable https://github.com/vatesfr/xo-web
-cd xo-server
-sudo npm install && npm run build
+sudo git clone -b stable https://github.com/vatesfr/xo-server
+sudo git clone -b stable https://github.com/vatesfr/xo-web
+sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+cd /opt/xo-server
+sudo yarn install --force
+cd /opt/xo-web
+sudo yarn install --force
 sudo cp sample.config.yaml .xo-server.yaml
 sudo sed -i /mounts/a\\"    '/': '/opt/xo-web/dist'" .xo-server.yaml
-cd /opt/xo-web
-sudo npm install
-sudo npm run build
-cat > /etc/systemd/system/xo-server.service <<EOF
-# systemd service for XO-Server.
-
-[Unit]
-Description= XO Server
-After=network-online.target
-
-[Service]
-WorkingDirectory=/opt/xo-server/
-ExecStart=/usr/local/bin/node ./bin/xo-server
-Restart=always
-SyslogIdentifier=xo-server
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
+sudo cp xo-server.service /etc/systemd/system/xo-server.service
+sudo sed -i 's/Environment=.*/WorkingDirectory=\/opt\/xo-server\//' /etc/systemd/system/xo-server.service
+sudo sed -i 's/ExecStart=.*/ExecStart=\/usr\/local\/bin\/node .\/bin\/xo-server\//' /etc/systemd/system/xo-server.service
 sudo chmod +x /etc/systemd/system/xo-server.service
 sudo systemctl enable xo-server.service
 sudo systemctl start xo-server.service
